a2373f0a60ec54fdf2e3c264f90c5c396430fe2d
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-01-25 23:04:21 -0500

    :rocket: cards spotify

--- src/views/NewReleases.vue
+++ src/views/NewReleases.vue
@@ -1,13 +1,26 @@
 <template>
     <h1 class="text-white text-3xl my-8 flex justify-center" >new releases</h1>  
-<div class=" bg-white w-11/12 flex m-auto my-12">
-    card
+<div class="w-11/12  m-auto my-12 grid grid-cols-6 ">
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
+<card-spotify/>
 </div>
 
 </template>
 
 <script lang="ts">
+import CardSpotify from '../components/CardSpotify.vue'
 export default {
+  components: { CardSpotify },
 
 }
 </script>
