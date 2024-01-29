dba1de6bdd41f0a69ff707e6063066d7f1687004
sebastian tovar <sebastiantovarchavez304@gmail.com>
2024-01-28 23:21:18 -0500

    :rocket: feat:api

--- src/views/NewReleases.vue
+++ src/views/NewReleases.vue
@@ -1,14 +1,12 @@
 <template>
-    <div>
-      <h1 class="text-white text-3xl my-8 flex justify-center">new releases</h1>
-      <div class="w-11/12 m-auto my-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-        <template v-for="(card, index) in cards" :key="index">
+  <h1 class="text-white text-3xl my-8 flex justify-center">new releases</h1>
+  <div class="w-11/12  m-auto my-12 grid grid-cols-6 ">
+      <template v-for="(card, index) in cards" :key="index">
           <card-spotify />
-        </template>
-      </div>
-    </div>
-  </template>
-  
+      </template>
+  </div>
+</template>
+
 
 <script lang="ts">
 import CardSpotify from '../components/CardSpotify.vue'
